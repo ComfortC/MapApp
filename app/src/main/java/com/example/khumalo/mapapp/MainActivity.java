dce7e7531501a156ab4118280e02f29c33c9013b
ComfortChinondiwana <comfortchino@gmail.com>
2016-07-09 15:53:55 +0200

    Map_4: Marker Options

--- app/src/main/java/com/example/khumalo/mapapp/MainActivity.java
+++ app/src/main/java/com/example/khumalo/mapapp/MainActivity.java
@@ -9,14 +9,39 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
 
-
+    GoogleMap m_map;
+    MarkerOptions Betram;
+    MarkerOptions District;
+    MarkerOptions South;
+    CameraPosition  CityCenter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LatLng Bertram_House  =  new LatLng(-33.930366, 18.413767);
+        Betram = new MarkerOptions().position(Bertram_House).title("Bertram House");
+        LatLng District_Six_Museum  =  new LatLng(-33.927544, 18.423731);
+        District= new MarkerOptions().position(District_Six_Museum).title("District Six Museum");
+        LatLng South_African_Museum =  new LatLng(-33.928738, 18.414925);
+        South= new MarkerOptions().position(South_African_Museum).title("South African Museum");
+        CityCenter = new CameraPosition.Builder()
+                .target(new LatLng(-33.928275878944675,18.423414580798408))
+                .bearing(0)
+                .tilt(45)
+                .zoom(15)
+                .build(); ;
 
        /* Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);*/
@@ -29,6 +54,12 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });*/
+
+
+
+        MapFragment mapFragment = (MapFragment) getFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
     }
 
     @Override
@@ -51,5 +82,14 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+           m_map = googleMap;
+           m_map.animateCamera(CameraUpdateFactory.newCameraPosition(CityCenter),20000,null);
+           m_map.addMarker(Betram);
+           m_map.addMarker(District);
+           m_map.addMarker(South);
     }
 }
